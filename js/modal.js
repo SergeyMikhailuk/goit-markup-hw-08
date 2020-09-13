72b1479c0f77342980260b98fb32be28918c2a95
Mikhailyuk Sergey <vzxyqw@gmail.com>
2020-09-13 11:24:50 +0300

    with flexible mobile design

--- js/modal.js
+++ js/modal.js
@@ -42,13 +42,19 @@ document.querySelectorAll('.form-input').forEach(element => {
   });
 });
 
-//Phone open/close munu
-(() => {
-  const menuBtn = document
-    .querySelector('[data-menu-button]')
-    .addEventListener('click', () => {
-      menuBtn.classList.toggle('is-open');
-    });
-})();
+//Menu
 
-//
+(() => {
+  const menuBtnRef = document.querySelector('[data-menu-button]');
+  const mobileMenuRef = document.querySelector('[data-menu]');
+
+  menuBtnRef.addEventListener('click', () => {
+    const expanded =
+      menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+
+    menuBtnRef.classList.toggle('is-open');
+    menuBtnRef.setAttribute('aria-expanded', !expanded);
+
+    mobileMenuRef.classList.toggle('is-open');
+  });
+})();
